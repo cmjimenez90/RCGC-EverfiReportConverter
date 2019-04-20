29444ee1faa39a25a1c147e4befa47545a3f9049
Carlos M Jimenez <developer@cmjimenez.com>
2019-04-20 00:47:30 -0400

    add EverfiUser model

---
+++ src/RCGC.EverfiReportConverter/Model/EverfiUser.cs
@@ -0,0 +1,11 @@
+namespace RCGC.EverfiReportConverter.Model
+{
+    public class EveriUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string EmployeId { get; set; }
+        public string Supervisor { get; set; }
+    }
+}
